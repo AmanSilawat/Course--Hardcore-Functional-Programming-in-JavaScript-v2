7ccc7edb864fb9b6f9f2f4f810350344a9f46324
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-12-25 23:07:05 +0530

    Added filter example

---
+++ curring/05-filter.js
@@ -0,0 +1,12 @@
+const add = (x, y) => x + y;
+const curry = f =>
+    x => y => f(x, y);
+
+const modulo = curry((x, y) => y % x);
+const isOdd = modulo(2);
+
+const filter = curry((f, xs) => xs.filter(f));
+const getOdds = filter(isOdd);
+const result = getOdds([1, 2, 3, 4, 5, 6]);
+
+console.log(result);
