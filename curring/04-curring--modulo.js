064a6390629b6fbb92e34b06edc717cab9af5076
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-12-25 22:27:05 +0530

    moduler example with curring

---
+++ curring/04-curring--modulo.js
@@ -0,0 +1,9 @@
+const add = (x, y) => x + y;
+const curry = f =>
+    x => y => f(x, y);
+
+const modulo = curry((x, y) => y % x);
+const isOdd = modulo(2);
+const result = isOdd(3);
+
+console.log(result);
