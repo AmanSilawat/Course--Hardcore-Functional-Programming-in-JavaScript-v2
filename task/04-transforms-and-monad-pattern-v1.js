003ae751d186e46294d8de873ffc1cfef2ba035d
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-12-29 16:38:11 +0530

    Added transform monad patt.. v1

---
+++ task/04-transforms-and-monad-pattern-v1.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { Task, Either, Id } = require('./types');
+const { Right, Left, fromNullable } = Either;
+const { List, Map } = require('immutable-ext');
+
+const httpGet = (path, params) =>
+    Task.of(`${path}: result`)
+
+const getUser = x => httpGet('/user', { id: x });
+const getTimeline = x => httpGet(`/timeline/${x}`, {});
+const getAds = () => httpGet('/ads', {});
+
+let res = List([getUser, getTimeline, getAds])
+    .traverse(Task.of, f => f())
+    .fork(console.log, x => console.log(x.toJS()))
